c41368349afb48707995c02f129fb9b0d2580481
TheMIG1122 <themig1122@gmail.com>
2021-10-02 02:23:27 +0500

    Starting Coding

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4db6ca1fb06561f8f73570ac4ac22945dc323265',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4db6ca1fb06561f8f73570ac4ac22945dc323265',
+    ),
+    'patreon/patreon' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '206575a16ce9a447b79b2367e1d4991ce76202d2',
+    ),
+  ),
+);
